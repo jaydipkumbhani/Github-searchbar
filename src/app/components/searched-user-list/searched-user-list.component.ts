a00c3acc3a925ffccbd44fa529c472d023682f6d
Jaydip Kumbhani <kumbhanijaydeep007@gmail.com>
2022-12-02 22:30:20 +0530

    work on search-user-list component and ready to serve

--- src/app/components/searched-user-list/searched-user-list.component.ts
+++ src/app/components/searched-user-list/searched-user-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-searched-user-list',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./searched-user-list.component.css']
 })
 export class SearchedUserListComponent {
+
+  @Input() users: Array<any> = [];
+  @Output() userSelected = new EventEmitter<any>();
+
+  selectUser(user: any) {
+    this.userSelected.emit({ userInfo: user.userInfo, topFiveRepos: user.topFiveRepos });
+  }
 
 }
